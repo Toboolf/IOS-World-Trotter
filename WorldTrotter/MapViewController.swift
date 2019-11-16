02a3c4c32ee75fbe5f9eefe8ec3054da990a4004
Jose Cruz <floobot@hotmail.com>
2019-11-16 13:54:45 -0600

    creating map view programmaticaly and a segmented control"

--- WorldTrotter/MapViewController.swift
+++ WorldTrotter/MapViewController.swift
@@ -7,12 +7,28 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
+  
+  var mapView: MKMapView!
   
   override func viewDidLoad() {
     super.viewDidLoad()
     print("Map view controller has loaded successfuly")
+  }
+  
+  override func loadView() {
+    //Creating a map view
+    mapView = MKMapView()
+    //Setting it as *the* view of this view controller
+    view = mapView
+    
+    let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satelite"])
+    segmentedControl.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+    segmentedControl.selectedSegmentIndex = 0
+    segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+    view.addSubview(segmentedControl)
   }
   
 }

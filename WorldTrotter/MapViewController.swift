7ad05462de07572f6ddc3683543f008cc44fbfea
Jose Cruz <floobot@hotmail.com>
2019-11-18 23:16:08 -0600

    changing mao type on selector change

--- WorldTrotter/MapViewController.swift
+++ WorldTrotter/MapViewController.swift
@@ -27,6 +27,9 @@ class MapViewController: UIViewController {
     let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satelite"])
     segmentedControl.backgroundColor = UIColor.white.withAlphaComponent(0.5)
     segmentedControl.selectedSegmentIndex = 0
+    
+    segmentedControl.addTarget(self, action: #selector(MapViewController.mapTypeChanged(_:)), for: .valueChanged)
+    
     segmentedControl.translatesAutoresizingMaskIntoConstraints = false
     view.addSubview(segmentedControl)
     
@@ -39,6 +42,20 @@ class MapViewController: UIViewController {
     leadingConstraint.isActive = true
     trailingConstraint.isActive = true
     
+  }
+  
+  @objc
+  func mapTypeChanged(_ segControl: UISegmentedControl) {
+    switch segControl.selectedSegmentIndex {
+    case 0:
+      mapView.mapType = .standard
+    case 1:
+      mapView.mapType = .hybrid
+    case 2:
+      mapView.mapType = .satellite
+    default:
+      break
+    }
   }
   
 }
